1f6118cd74cc5f0f613e1860a2af438301bae3f8
Raphael Groemmer <mail@rapsn.me>
2023-09-05 22:28:08 +0200

    Enter the void

--- config.nix
+++ config.nix
@@ -1,58 +1,8 @@
 { pkgs }: {
   extraPackages = with pkgs; [
-    gopls
-    gotools
-    golangci-lint-langserver
-    rnix-lsp
-    terraform-ls
-    rust-analyzer
-    nodePackages.yaml-language-server
-    nodejs
-
-    tree-sitter
-    python39
-    sqlite
-
-    fzf
-    bat
-    ripgrep
   ];
 
   startPlugins = with pkgs.vimPlugins; [
-    onedark-nvim
-    nvim-tree-lua      # explorer
-    nvim-lspconfig      # lsp-config
-
-    nvim-treesitter.withAllGrammars
-
-    nvim-cmp
-    cmp-nvim-lsp
-    cmp-path
-    cmp-buffer
-    cmp-cmdline
-    cmp-git
-
-    cmp-nvim-ultisnips
-    ultisnips
-
-    vim-snippets
-    cmp-vsnip
-    vim-vsnip
-    friendly-snippets
-
-    lsp_signature-nvim
-
-    which-key-nvim
-    nvim-autopairs
-    bufferline-nvim
-    lualine-nvim
-    indent-blankline-nvim
-
-    plenary-nvim
-    telescope-nvim
-
-    nvim-ts-rainbow 
-    vim-illuminate
   ];
 
   optPlugins = with pkgs; [
